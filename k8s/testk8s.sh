a8542f364734b7a6ab0b0987cc4dfd7367dc18ad
Jim Bromfield <jbromfld@gmail.com>
2021-07-19 12:38:44 -0400

    testing helm

---
+++ k8s/testk8s.sh
@@ -0,0 +1,8 @@
+#! /usr/bin#! /usr/bin
+
+kubectl apply -f consul.yaml
+kubectl apply -f gatway.yaml
+kubectl apply -f frontend.yaml
+kubectl apply -f account.yaml
+kubectl apply -f project.yaml
+kubectl apply -f tracking.yaml
