b0dd561f7e1cce3e8b70fb4759295996dbbe127f
Jim Bromfield <jbromfld@gmail.com>
2021-07-19 12:51:10 -0400

    testing k8s

--- k8s/zztestk8s.sh
+++ k8s/zztestk8s.sh
@@ -2,7 +2,7 @@
 
 kubectl apply -f configmap.yaml
 kubectl apply -f consul.yaml
-kubectl apply -f gatway.yaml
+kubectl apply -f gateway.yaml
 kubectl apply -f frontend.yaml
 kubectl apply -f account.yaml
 kubectl apply -f project.yaml
