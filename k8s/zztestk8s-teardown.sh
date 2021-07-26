b8f10ec736798121d544518c9781203619fa631d
Jacob Bulloch <jacob.bulloch@revature.net>
2021-07-26 11:00:40 -0600

    remove unused scripts

--- k8s/zztestk8s-teardown.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/bash
-
-kubectl delete -f configmap.yaml
-kubectl delete -f consul.yaml
-kubectl delete -f gateway.yaml
-kubectl delete -f frontend.yaml
-kubectl delete -f account.yaml
-kubectl delete -f project.yaml
-kubectl delete -f tracking.yaml
--- k8s/zztestk8s.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-kubectl apply -f configmap.yaml
-kubectl apply -f consul.yaml
-kubectl apply -f gateway.yaml
-kubectl apply -f frontend.yaml
-kubectl apply -f account.yaml
-kubectl apply -f project.yaml
-kubectl apply -f tracking.yaml
